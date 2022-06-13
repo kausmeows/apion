c752ebb9f982b2577f237f8ba8ed292a3ff5e131
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-06-14 01:34:42 +0530

    add and remove key value pair in query params and headers

--- script.js
+++ script.js
@@ -1,2 +1,29 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+const queryParamsContainer = document.querySelector('[data-query-params]');
+const requestHeaderContainer = document.querySelector('[data-request-headers]');
+const keyValueTemplate = document.querySelector("[data-key-value-template]");
+
+document
+    .querySelector('[data-add-query-param-btn]')
+    .addEventListener('click', () => {
+        queryParamsContainer.append(createKeyValuePair());
+})
+
+document
+  .querySelector("[data-add-request-header-btn]")
+  .addEventListener("click", () => {
+    requestHeaderContainer.append(createKeyValuePair());
+  });
+
+queryParamsContainer.append(createKeyValuePair())
+requestHeaderContainer.append(createKeyValuePair())
+
+function createKeyValuePair() {
+    const element = keyValueTemplate.content.cloneNode(true);
+    element.querySelector('[data-remove-btn]').addEventListener('click', (e) => {
+        e.target.closest('[data-key-value-pair]').remove()
+    })
+    return element;
+}
