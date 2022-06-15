3cac28b8cf5051fbc526e35da1ab3e55744258de
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-06-15 22:03:04 +0530

    configure the axios for api calls

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
 
+const form = document.querySelector('[data-form]')
 const queryParamsContainer = document.querySelector('[data-query-params]');
-const requestHeaderContainer = document.querySelector('[data-request-headers]');
+const requestHeadersContainer = document.querySelector('[data-request-headers]');
 const keyValueTemplate = document.querySelector("[data-key-value-template]");
 
 document
@@ -14,11 +16,24 @@ document
 document
   .querySelector("[data-add-request-header-btn]")
   .addEventListener("click", () => {
-    requestHeaderContainer.append(createKeyValuePair());
+    requestHeadersContainer.append(createKeyValuePair());
   });
 
 queryParamsContainer.append(createKeyValuePair())
-requestHeaderContainer.append(createKeyValuePair())
+requestHeadersContainer.append(createKeyValuePair())
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+
+  axios({
+    url: document.querySelector("[data-url]").value,
+    method: document.querySelector("[data-method]").value,
+    params: keyValuePairsToObjects(queryParamsContainer),
+    headers: keyValuePairsToObjects(requestHeadersContainer),
+  }).then(response => {
+    console.log(response);
+  })
+})
 
 function createKeyValuePair() {
     const element = keyValueTemplate.content.cloneNode(true);
@@ -26,4 +41,15 @@ function createKeyValuePair() {
         e.target.closest('[data-key-value-pair]').remove()
     })
     return element;
+}
+
+function keyValuePairsToObjects(container) {
+  const pairs = container.querySelectorAll('[data-key-value-pair]')
+  return [...pairs].reduce((data, pair) => {
+    const key = pair.querySelector('[data-key]').value
+    const value = pair.querySelector('[data-value]').value
+
+    if(key === '') return data
+    return { ...data, [key]: value }
+  }, {})
 }
