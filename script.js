fa65a5b968573f49d3c8fe0b3596afbc4c39c590
kaustubh-s1 <shuklakaustubh84@gmail.com>
2022-06-18 02:27:54 +0530

    add response javascript

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
+import prettyBytes from 'pretty-bytes';
 
 const form = document.querySelector('[data-form]')
 const queryParamsContainer = document.querySelector('[data-query-params]');
 const requestHeadersContainer = document.querySelector('[data-request-headers]');
 const keyValueTemplate = document.querySelector("[data-key-value-template]");
+const responseHeadersContainer = document.querySelector('[data-response-headers]');
 
 document
     .querySelector('[data-add-query-param-btn]')
@@ -22,6 +24,23 @@ document
 queryParamsContainer.append(createKeyValuePair())
 requestHeadersContainer.append(createKeyValuePair())
 
+axios.interceptors.request.use((request) => {
+  request.customData = request.customData || {};
+  request.customData.startTime = new Date().getTime();
+  return request;
+});
+
+function updateEndTime(response) {
+  response.customData = response.customData || {};
+  response.customData.time =
+    new Date().getTime() - response.config.customData.startTime;
+  return response;
+}
+
+axios.interceptors.response.use(updateEndTime, (e) => {
+  return Promise.reject(updateEndTime(e.response));
+});
+
 form.addEventListener('submit', e => {
   e.preventDefault()
 
@@ -30,10 +49,36 @@ form.addEventListener('submit', e => {
     method: document.querySelector("[data-method]").value,
     params: keyValuePairsToObjects(queryParamsContainer),
     headers: keyValuePairsToObjects(requestHeadersContainer),
-  }).then(response => {
+  })
+  .catch(e => e)
+  .then(response => {
+    document.querySelector('[data-response-section]').classList.remove("d-none")
+    updateResponseDetails(response)
+    updateResponseHeaders(response.headers)
     console.log(response);
   })
 })
+
+function updateResponseDetails(response) {
+  document.querySelector('[data-status]').textContent = response.status
+  document.querySelector('[data-time]').textContent = response.customData.time
+
+  document.querySelector('[data-size]').textContent = prettyBytes(
+    JSON.stringify(response.data).length + JSON.stringify(response.headers).length
+  )
+}
+
+function updateResponseHeaders(headers) {
+  responseHeadersContainer.innerHTML = ""
+  Object.entries(headers).forEach(([key, value]) => {
+    const keyElement = document.createElement('div');
+    keyElement.textContent = key;
+    responseHeadersContainer.append(keyElement);
+    const valueElement = document.createElement("div");
+    valueElement.textContent = value;
+    responseHeadersContainer.append(valueElement);
+  })
+}
 
 function createKeyValuePair() {
     const element = keyValueTemplate.content.cloneNode(true);
